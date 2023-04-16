2372c8b74af8766c5b538c71215582088d29fdba
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2023-04-16 12:23:11 +0600

    Header File

---
+++ superior-wp-testimonial.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Superior WP Testimonial
+ * Plugin URI:        https://wordpress.org/plugins/superior-wp-testimonial/
+ * Description:       Superior WP Testimonial is a plugin for displaying the client's review on the website.
+ * Version:           1.0
+ * Requires at least: 6.0
+ * Requires PHP:      7.2
+ * Author:            Md. Ibrahim Khalil
+ * Author URI:        https://www.ibrahimkhalil.xyz/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       spwptm
+ */
+
+?>
