bf3af649ba89bbfd5608de95119229379e827407
Md. Ibrahim Khalil <mdibrahimk48@gmail.com>
2023-04-16 13:57:07 +0600

    Plugin Style CSS File done

--- superior-wp-testimonial.php
+++ superior-wp-testimonial.php
@@ -20,9 +20,9 @@
  */
 
 function spwptm_enqueue_styles() {
-	wp_enqueue_style( 'owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css' );
-	wp_enqueue_style( 'owl-theme', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css' );
-	wp_enqueue_style( 'testimonial-css', plugins_url( 'css/testimonial.css', __FILE__) );
+	wp_enqueue_style( 'spwptm-owl-carousel', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css' );
+	wp_enqueue_style( 'spwptm-owl-theme', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css' );
+	wp_enqueue_style( 'spwptm-style', plugins_url( 'css/spwptm_style.css', __FILE__) );
 }
 add_action( 'wp_enqueue_scripts', 'spwptm_enqueue_styles' );
 
